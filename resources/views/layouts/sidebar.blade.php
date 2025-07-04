809ba6e22c498997e5eb01ef346d7cc01a9dfd58
Iqbal Andreas Syahputa <iqbal.and303@gmail.com>
2025-07-04 14:32:45 +0700

    impementasi Authentication

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -87,6 +87,13 @@
                     </li>
                 </ul>
             </li>
-        </ul>
+
+            <li class="nav-item">
+                <a href="{{ url('logout') }}" class="nav-link">
+                    <i class="nav-icon fas fa-sign-out-alt"></i>
+                    <p>Logout</p>
+                </a>
+            </li>
+            </ul>
     </nav>
 </div>
